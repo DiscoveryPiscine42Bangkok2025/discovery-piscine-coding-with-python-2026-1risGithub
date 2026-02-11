2e60738bbd62ba5ac5efc6a9e1cc74b4c14cc48d
Iris <131670661+1risGithub@users.noreply.github.com>
2026-02-11 21:20:12 +0700

    ex09 Done

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,15 @@
+import sys
+
+args = sys.argv[1:]
+
+if len(args) != 2:
+    print("none")
+else:
+    keyword = args[0]
+    sentence = args[1]
+    count = sentence.count(keyword)
+
+    if count == 0:
+        print("none")
+    else:
+        print(count)
